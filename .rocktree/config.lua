4de1f88ffb442a53906aca75e944b86d2111ed76
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2018-10-17 18:56:24 -0300

    Make commands for main wagon script
    We now have:
    + usage
    + load (which requires source)
    + intall

--- .rocktree/config.lua
+++
@@ -1,1 +0,0 @@
-rocks_trees = { { name='user', root='/home/kazuo256/Workspace/projects/lux-archive/wagon/.rocktree' } }
--- example/run.sh
+++ example/run.sh
@@ -2,9 +2,13 @@
 
 echo "This is an example using Lua 5.3"
 
-source ../bin/wagon 5.3
+source ../bin/wagon load 5.3
 
-lua ../bin/wagon-deps.lua example.rockspec
+pushd ..
+luarocks make
+popd
+
+wagon install example.rockspec
 
 lua example.lua && echo "Success!"
 
