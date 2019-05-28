536150aade612b064fbd425b260f9cedcb5b9b78
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2019-05-28 10:46:29 -0300

    Change path concatenation to avoid double slash

--- lua/wagon/bundler.lua
+++ lua/wagon/bundler.lua
@@ -5,15 +5,15 @@ local BUNDLER = {}
 
 function BUNDLER.bundle()
   local major, minor = _VERSION:match("(%d+)%.(%d+)")
-  local version_folder = major .. "." .. minor .. "/"
+  local version_folder = '/' .. major .. '.' .. minor
   local env = {}
   env.version = { tonumber(major), tonumber(minor) }
   env.lua_paths = {
-    DEFS.LUA_DIR .. version_folder .. "?.lua",
-    DEFS.LUA_DIR .. version_folder .. "?/init.lua",
+    DEFS.LUA_DIR .. version_folder .. "/?.lua",
+    DEFS.LUA_DIR .. version_folder .. "/?/init.lua",
   }
   env.lib_paths = {
-    DEFS.LIB_DIR .. version_folder .. "?.so"
+    DEFS.LIB_DIR .. version_folder .. "/?.so"
   }
   env.bin_paths = {
     DEFS.BIN_DIR
