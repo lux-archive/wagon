149a178c748f65f91766738118a3fa48478a8771
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2020-05-28 21:34:04 -0300

    Add current dir to bundled paths

--- lua/wagon/bundler.lua
+++ lua/wagon/bundler.lua
@@ -10,9 +10,12 @@ function BUNDLER.bundle()
   env.lua_paths = {
     DEFS.LUA_DIR .. version_folder .. "/?.lua",
     DEFS.LUA_DIR .. version_folder .. "/?/init.lua",
+    "?.lua",
+    "?/init.lua",
   }
   env.lib_paths = {
-    DEFS.LIB_DIR .. version_folder .. "/?.so"
+    DEFS.LIB_DIR .. version_folder .. "/?.so",
+    "?.so",
   }
   env.bin_paths = {
     DEFS.BIN_DIR
